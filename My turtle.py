fbd52f9d8a88981c3c37a881c9e12811dfdfd567
amaddix2020 <74970104+amaddix2020@users.noreply.github.com>
2020-11-25 12:46:21 +0000

    Python Script
    Python Script to draw a square

---
+++ My turtle.py
@@ -0,0 +1,14 @@
+import turtle
+
+# Make a square with turtle script.
+My_turtle = turtle.Turtle()
+
+def square():
+    My_turtle.forward(100)
+    My_turtle.right(90)
+    My_turtle.forward(100)
+    My_turtle.right(90)
+    My_turtle.forward(100)
+    My_turtle.right(90)
+    My_turtle.forward(100)
+square()
